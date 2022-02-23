b49deeae5c2d58765d30031d867b145468cea75c
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2022-02-23 17:12:33 +0800

    Fix style errors

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates the remark object
+     * @param remark The String remark to be passed in.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
