e08b4e451c121fb97f4c0c44d1c5c14b53d260ec
aditi2313 <guptaaditi2313@gmail.com>
2022-03-10 01:17:17 +0800

    Valid Name and Email checks.

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain upto a max of 40 alphanumeric characters and spaces, and should not be blank.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+//    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String fullName;
 
@@ -35,7 +35,9 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+//        return test.matches(VALIDATION_REGEX);
+        String[] nameArray = test.split("");
+        return (nameArray.length < 40);
     }
 
 
