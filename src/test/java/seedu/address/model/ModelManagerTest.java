84c998897c494619bdccd504b115f1c963193f06
Lim Bing Sen <bingsenlim0806@gmail.com>
2022-03-10 12:40:43 +0800

    Storage and Model Component: Minor Refactoring and Adding Tests
    getTypicalAddressBook() is a method that is used for many test cases,
    and is located inside TypicalPersons file.
    
    It does not make sense to be inside that file because
    getTypicalAddressBook() also needs to initialize the books, which are
    inside TypicalBooks file.
    
    Furthermore, the higher level Storage and Model components are yet to be
    tested properly after the Book adaptation. Some test data json files are
    also outdated.
    
    Let's,
    * Refactor getTypicalAddressBook() into a new TypicalAddressBook file,
    and update all references to the method call in all test classes
    * Update the test json data to include books
    * Complete test classes for all Storage and Model components to provide
    better test coverage
    
    Updating test json data will prevent us the Storage component when
    throwing error during testing. This is because the storage component
    throws error when it tries to read book data from our sample data, but
    the sample data does not contain "books" field.

--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -3,8 +3,10 @@ package seedu.address.model;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_BOOKS;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalBooks.HARRY_POTTER;
 import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 
@@ -15,6 +17,7 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.GuiSettings;
+import seedu.address.model.book.BookNameContainsKeywordsPredicate;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.testutil.AddressBookBuilder;
 
@@ -78,8 +81,18 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void hasBook_nullBook_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.hasBook(null));
+    }
+
+    @Test
     public void hasPerson_personNotInAddressBook_returnsFalse() {
         assertFalse(modelManager.hasPerson(ALICE));
+    }
+
+    @Test
+    public void hasBook_bookNotInAddressBook_returnsFalse() {
+        assertFalse(modelManager.hasBook(HARRY_POTTER));
     }
 
     @Test
@@ -89,8 +102,19 @@ public class ModelManagerTest {
     }
 
     @Test
+    public void hasBook_bookInAddressBook_returnsTrue() {
+        modelManager.addBook(HARRY_POTTER);
+        assertTrue(modelManager.hasBook(HARRY_POTTER));
+    }
+
+    @Test
     public void getFilteredPersonList_modifyList_throwsUnsupportedOperationException() {
         assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredPersonList().remove(0));
+    }
+
+    @Test
+    public void getFilteredBookList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredBookList().remove(0));
     }
 
     @Test
@@ -116,13 +140,21 @@ public class ModelManagerTest {
         // different addressBook -> returns false
         assertFalse(modelManager.equals(new ModelManager(differentAddressBook, userPrefs)));
 
-        // different filteredList -> returns false
+        // different filteredPersonList -> returns false
         String[] keywords = ALICE.getName().fullName.split("\\s+");
         modelManager.updateFilteredPersonList(new NameContainsKeywordsPredicate(Arrays.asList(keywords)));
         assertFalse(modelManager.equals(new ModelManager(addressBook, userPrefs)));
 
+        // different filteredBookList -> returns false
+        String[] bookKeywords = HARRY_POTTER.getBookName().fullBookName.split("\\s+");
+        modelManager.updateFilteredBookList(new BookNameContainsKeywordsPredicate(Arrays.asList(bookKeywords)));
+        assertFalse(modelManager.equals(new ModelManager(addressBook, userPrefs)));
+
         // resets modelManager to initial state for upcoming tests
         modelManager.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        // resets modelManager to initial state for upcoming tests
+        modelManager.updateFilteredBookList(PREDICATE_SHOW_ALL_BOOKS);
 
         // different userPrefs -> returns false
         UserPrefs differentUserPrefs = new UserPrefs();
