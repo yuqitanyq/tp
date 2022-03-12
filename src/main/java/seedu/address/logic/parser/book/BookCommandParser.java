06cd96cc02c2de6f1b1467b71a69b21c8d4bfd11
Yu Qi Tan <83687861+yuqitanyq@users.noreply.github.com>
2022-03-12 15:05:31 +0800

    Fix CheckStyle to make sure styling is consistent.

--- src/main/java/seedu/address/logic/parser/book/BookCommandParser.java
+++ src/main/java/seedu/address/logic/parser/book/BookCommandParser.java
@@ -8,7 +8,6 @@ import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.book.EditBookCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 
