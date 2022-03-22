95b3b273c17bac64e9440ab1781faa8645d681f7
yl-ang <yongliangang@gmail.com>
2022-03-22 12:37:08 +0800

    Add test cases for RelatedBookCommand class

--- src/test/java/seedu/address/model/book/BookRelatedToPatronPredicateTest.java
+++ src/test/java/seedu/address/model/book/BookRelatedToPatronPredicateTest.java
@@ -1,7 +1,7 @@
 package seedu.address.model.book;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
